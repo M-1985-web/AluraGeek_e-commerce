bdbe6f289b86890f4f09643efc6f0597dfd0397f
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-09-01 17:40:55 -0300

    heroku

---
+++ services/client-service.js
@@ -0,0 +1,49 @@
+//heroku
+
+const createProduct = async (jsonProduct) => {
+    try {
+        const response = await fetch(`https://git.heroku.com/aluralatamecommerce.git/products`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonProduct
+        });
+        return response.json;
+    } catch (error) {
+        return console.log(error);
+    }
+}
+
+const readProducts = () => fetch('https://git.heroku.com/aluralatamecommerce.git/products').then
+(response => response.json());
+
+const readProduct = (id) => fetch(`https://git.heroku.com/aluralatamecommerce.git/products/${id}`).
+then(response => response.json());
+
+const deleteProduct = (id) => {
+    return fetch(`https://git.heroku.com/aluralatamecommerce.git/products/${id}`, {
+        method: 'DELETE'
+    });
+};
+
+const updateProduct = (id, jsonProduct) => {
+    return fetch(`https://git.heroku.com/aluralatamecommerce.git/products/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonProduct
+    })
+    .then(response => response.json)
+    .catch(error => console.log(error))
+}
+
+
+export const clientService = {
+    createProduct,
+    readProducts,
+    readProduct,
+    updateProduct,
+    deleteProduct,
+};
